74ce89ae48d2bc5dda702d5d99b73e1a19eeeef0
Bharath C05 <Bharath_C05@ad.infosys.com>
2020-03-07 22:50:21 +0000

    terraform for jenkins11

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="annular-sky-267913"
+project="bh-demo-test"
--- modules/gcs/main.tf
+++ modules/gcs/main.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "vm-mat-raw-data" {
-  name     = "vm-mat-raw-data-${var.env}"
+resource "google_storage_bucket" "vm-mat-raw-data1" {
+  name     = "vm-mat-raw-data-${var.env}-1"
   location = "EU"
   project = "${var.project}"
   }
