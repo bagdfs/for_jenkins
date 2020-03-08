ca1ba0c6c5c7f333223331723720c7e29e4da835
Bharath C05 <Bharath_C05@ad.infosys.com>
2020-03-08 06:15:03 +0000

    terraform for jenkins17

---
+++ modules/bigquery/main.tf
@@ -0,0 +1,33 @@
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "vm-mat-bq-raw-layer-${var.env}"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+
+  access {
+    role          = "OWNER"
+    user_by_email = "bharathchandrasekar.90@gmail.com"
+  }
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "vm-mat-bq-staging-layer-${var.env}"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+
+  access {
+    role          = "OWNER"
+    user_by_email = "bharathchandrasekar.90@gmail.com"
+  }
+}
--- modules/gcs/main.tf
+++ modules/gcs/main.tf
@@ -1,6 +1,19 @@
-resource "google_storage_bucket" "vm-mat-raw-data1" {
-  name     = "vm-mat-raw-data-${var.env}-1"
+resource "google_storage_bucket" "vm-mat-raw-data" {
+  name     = "vm-mat-raw-data-${var.env}"
   location = "EU"
   project = "${var.project}"
   }
+
+resource "google_storage_bucket" "vm-mat-trans-data" {
+  name     = "vm-mat-raw-trans-${var.env}"
+  location = "EU"
+  project = "${var.project}"
+  }  
   
+resource "google_storage_bucket" "vm-mat-agg-data" {
+  name     = "vm-mat-raw-agg-${var.env}"
+  location = "EU"
+  project = "${var.project}"
+  }  
+
+ 
